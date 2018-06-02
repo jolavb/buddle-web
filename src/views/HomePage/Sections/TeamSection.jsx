522e71df53ae0684d555e3b1e6d8897cc3f21a47
jolavb <bartholdjulian@gmail.com>
2018-06-02 15:47:39 -0400

    Add TeamSection Componenet

--- src/views/HomePage/Sections/TeamSection.jsx
+++ src/views/HomePage/Sections/TeamSection.jsx
@@ -16,9 +16,9 @@ import IconButton from "components/CustomButtons/IconButton.jsx";
 
 import teamStyle from "assets/jss/material-kit-react/views/landingPageSections/teamStyle.jsx";
 
-import team1 from "assets/img/faces/avatar.jpg";
-import team2 from "assets/img/faces/christian.jpg";
-import team3 from "assets/img/faces/kendall.jpg";
+import team1 from "assets/img/faces/SaraProfile.png";
+import team2 from "assets/img/faces/JulieProfile.png";
+import team3 from "assets/img/faces/JulianProfile.png";
 
 class TeamSection extends React.Component {
   render() {
@@ -30,7 +30,7 @@ class TeamSection extends React.Component {
     );
     return (
       <div className={classes.section}>
-        <h2 className={classes.title}>Here is our team</h2>
+        <h2 className={classes.title}>Our Team</h2>
         <div>
           <GridContainer>
             <GridItem xs={12} sm={12} md={4}>
@@ -39,16 +39,13 @@ class TeamSection extends React.Component {
                   <img src={team1} alt="..." className={imageClasses} />
                 </GridItem>
                 <h4 className={classes.cardTitle}>
-                  Gigi Hadid
+                  Sarah Gersten
                   <br />
-                  <small className={classes.smallTitle}>Model</small>
+                  <small className={classes.smallTitle}>Chief Executive Officer</small>
                 </h4>
                 <CardBody>
                   <p className={classes.description}>
-                    You can write here details about one of your team members.
-                    You can give more details about what they do. Feel free to
-                    add some <a href="#pablo">links</a> for people to be able to
-                    follow them outside the site.
+                    Sarah is a transactional attorney with a focus on intellectual property.  While she currently works for a congressional agency, she has experience working with public and private companies in initial and secondary public offerings, and has advised companies on issues of formation, operation, and general corporate matters.  She became involved in cannabis law and policy during law school when she worked with the Marijuana Policy Project of Massachusetts on the initiative to legalize recreational marijuana.  She currently serves as a founding member of the DC Cannabis Business Association.  Sarah is a graduate of Tulane University and Harvard Law School.
                   </p>
                 </CardBody>
                 <CardFooter className={classes.justifyCenter}>
@@ -70,16 +67,21 @@ class TeamSection extends React.Component {
                   <img src={team2} alt="..." className={imageClasses} />
                 </GridItem>
                 <h4 className={classes.cardTitle}>
-                  Christian Louboutin
+                  Julie Saltman
                   <br />
-                  <small className={classes.smallTitle}>Designer</small>
+                  <small className={classes.smallTitle}>Chief Operating Officer</small>
                 </h4>
                 <CardBody>
                   <p className={classes.description}>
-                    You can write here details about one of your team members.
-                    You can give more details about what they do. Feel free to
-                    add some <a href="#pablo">links</a> for people to be able to
-                    follow them outside the site.
+                  Julie Saltman is a seasoned attorney who spent over a decade with
+                  the United States Department of Justice, Civil Division, before becoming
+                  Assistant General Counsel at the U.S. Copyright Office. Her work at DOJ focused primarily
+                  on litigation involving federal statutes, regulations, and agency actions in cases involving
+                  matters of constitutional and administrative law.
+                  As Assistant General Counsel, Julie currently assists the Copyright Office in ongoing litigation,
+                  congressionally mandated rulemakings and regulatory requirements, as well as drafting and reviewing
+                  office policies and procedures.  Julie is also a member of the Georgetown Law adjunct faculty.
+                  She is a graduate of Yale University and University of Michigan Law School.
                   </p>
                 </CardBody>
                 <CardFooter className={classes.justifyCenter}>
@@ -98,16 +100,20 @@ class TeamSection extends React.Component {
                   <img src={team3} alt="..." className={imageClasses} />
                 </GridItem>
                 <h4 className={classes.cardTitle}>
-                  Kendall Jenner
+                  Julian Barthold
                   <br />
-                  <small className={classes.smallTitle}>Model</small>
+                  <small className={classes.smallTitle}>Chief Technology Officer</small>
                 </h4>
                 <CardBody>
                   <p className={classes.description}>
-                    You can write here details about one of your team members.
-                    You can give more details about what they do. Feel free to
-                    add some <a href="#pablo">links</a> for people to be able to
-                    follow them outside the site.
+                  Julian is a former Oracle IT Auditor whose side projects turned
+                  into a passion for  building web infrastructure to explore and process data.
+                  He began with Visual Basic and shell scripting, building programs to automate
+                  repetitive audit data analysis.  Julian continues to broaden his understanding of
+                  software architecture through contract work with companies like Snomatic, where he is currently
+                  developing snowmaking automation and control integration.  Julian is a graduate of Boston University
+                   and the General Assembly Data Science and Web Development programs where he learned to build web
+                   infrastructure around data.
                   </p>
                 </CardBody>
                 <CardFooter className={classes.justifyCenter}>
